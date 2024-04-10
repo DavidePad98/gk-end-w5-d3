609ae10d70a77f5bd789b50569e23d0f73ac4f05
Davide Paderi 98 <151513202+DavidePad98@users.noreply.github.com>
2024-04-10 15:01:49 +0200

    start app

---
+++ src/main/java/bkendw5d3/w5d3/W5D3Application.java
@@ -0,0 +1,13 @@
+package bkendw5d3.w5d3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W5D3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W5D3Application.class, args);
+	}
+
+}
---
+++ src/test/java/bkendw5d3/w5d3/W5D3ApplicationTests.java
@@ -0,0 +1,13 @@
+package bkendw5d3.w5d3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W5D3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
